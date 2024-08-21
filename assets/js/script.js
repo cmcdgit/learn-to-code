b20f74d8425d19d2220e2a84da33983375bd44b9
Ciaran Mc Devitt <cmcdgit@gmail.com>
2024-08-21 19:36:05 +0000

    Hook up book recommendations to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,11 @@ import { cssQuestions } from "../questions/css-questions.js";
 import { javascriptQuestions } from "../questions/javascript-questions.js";
 import { cPlusPlusQuestions } from "../questions/cPlusPlus-questions.js";
 
+import { pythonBookRecommendations } from "../recommended-books/python-book-recommendations.js";
+import { cssBookRecommendations } from "../recommended-books/css-book-recommendations.js";
+import { javascriptBookRecommendations } from "../recommended-books/javascript-book-recommendations.js";
+import { cPlusPlusBookRecommendations } from "../recommended-books/cPlusPlus-book-recommendations.js";
+
 document.addEventListener("DOMContentLoaded", function() {
     let options = document.getElementsByTagName('button');
 
@@ -35,22 +40,26 @@ function takePythonQuiz(questionNum) {
     console.log(questionNum);
     console.log(pythonQuestions[questionNum]);
     console.log(pythonQuestions);
+    console.log(pythonBookRecommendations);
 }
 
 function takeCSSQuiz(questionNum) {
     console.log(questionNum);
     console.log(cssQuestions[questionNum]);
     console.log(cssQuestions);
+    console.log(cssBookRecommendations);
 }
 
 function takeJavaScriptQuiz(questionNum) {
     console.log(questionNum);
     console.log(javascriptQuestions[questionNum]);
     console.log(javascriptQuestions);
+    console.log(javascriptBookRecommendations);
 }
 
 function takeCPPQuiz(questionNum) {
     console.log(questionNum);
     console.log(cPlusPlusQuestions[questionNum]);
     console.log(cPlusPlusQuestions);
+    console.log(cPlusPlusBookRecommendations);
 }
---
+++ assets/recommended-books/css-book-recommendations.js
@@ -0,0 +1,82 @@
+export const cssBookRecommendations = [
+    {
+        "title": "CSS: The Definitive Guide",
+        "author": "Eric A. Meyer and Estelle Weyl",
+        "description": "This comprehensive guide covers everything from the basics of CSS to advanced techniques. It is a go-to reference for developers looking to master CSS.",
+        "best_for": "Intermediate to advanced developers who want a thorough understanding of CSS.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1449393195",
+        "cover_image": "https://m.media-amazon.com/images/I/51ffxv0QG8L._SX379_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "Learning Web Design: A Beginner's Guide to HTML, CSS, JavaScript, and Web Graphics",
+        "author": "Jennifer Niederst Robbins",
+        "description": "This book is a beginner-friendly guide to web design, covering HTML, CSS, JavaScript, and web graphics. It provides a solid foundation for anyone new to web development.",
+        "best_for": "Beginners who want a comprehensive introduction to web design, including CSS.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1491960205",
+        "cover_image": "https://m.media-amazon.com/images/I/41bTkPE1V9L._SX379_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "CSS Secrets: Better Solutions to Everyday Web Design Problems",
+        "author": "Lea Verou",
+        "description": "This book provides 47 tips and techniques for solving common web design problems with CSS. It is packed with practical advice for creating innovative and efficient CSS solutions.",
+        "best_for": "Intermediate to advanced developers looking for practical tips and tricks to enhance their CSS skills.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1449372635",
+        "cover_image": "https://m.media-amazon.com/images/I/51sLda9a+vL._SX379_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "HTML and CSS: Design and Build Websites",
+        "author": "Jon Duckett",
+        "description": "This beautifully designed book provides a visual introduction to HTML and CSS. It is easy to follow and perfect for beginners who want to learn web development.",
+        "best_for": "Beginners who prefer a visually engaging introduction to HTML and CSS.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1118008189",
+        "cover_image": "https://m.media-amazon.com/images/I/41-7giKufNL._SX397_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "Transcending CSS: The Fine Art of Web Design",
+        "author": "Andy Clarke",
+        "description": "This book goes beyond the basics, offering insights into advanced CSS techniques and modern web design practices. It focuses on creating visually appealing, standards-compliant designs.",
+        "best_for": "Experienced designers and developers looking to elevate their CSS and design skills.",
+        "amazon_link": "https://www.amazon.co.uk/dp/0321410971",
+        "cover_image": "https://m.media-amazon.com/images/I/41syWo22PmL._SX404_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "CSS3: The Missing Manual",
+        "author": "David Sawyer McFarland",
+        "description": "This book is a comprehensive guide to CSS3, covering everything from basic syntax to advanced layout techniques. It is a great resource for learning modern CSS.",
+        "best_for": "Developers who want to learn or upgrade their skills to CSS3.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1491918055",
+        "cover_image": "https://m.media-amazon.com/images/I/51qlaLhNyTL._SX379_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "Responsive Web Design with HTML5 and CSS",
+        "author": "Ben Frain",
+        "description": "This book focuses on building responsive websites using HTML5 and CSS. It covers modern web development techniques and best practices.",
+        "best_for": "Developers looking to master responsive design with modern HTML and CSS.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1784398931",
+        "cover_image": "https://m.media-amazon.com/images/I/51hApx8ZucL._SX379_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "CSS Mastery: Advanced Web Standards Solutions",
+        "author": "Andy Budd, Emil Björklund",
+        "description": "This book is a deep dive into advanced CSS techniques and best practices. It covers modern CSS methods and provides solutions to common web design challenges.",
+        "best_for": "Experienced developers who want to master advanced CSS techniques.",
+        "amazon_link": "https://www.amazon.co.uk/dp/1430258630",
+        "cover_image": "https://m.media-amazon.com/images/I/51Vm1HlU1GL._SX379_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "Pro CSS Techniques",
+        "author": "Jeff Croft, Ian Lloyd, Dan Rubin",
+        "description": "This book covers professional-level CSS techniques for creating sophisticated, standards-compliant websites. It is perfect for developers looking to take their CSS skills to the next level.",
+        "best_for": "Advanced developers seeking to refine their professional CSS skills.",
+        "amazon_link": "https://www.amazon.co.uk/dp/159059732X",
+        "cover_image": "https://m.media-amazon.com/images/I/51X5BbSP-ML._SX396_BO1,204,203,200_.jpg"
+    },
+    {
+        "title": "Designing with Web Standards",
+        "author": "Jeffrey Zeldman, Ethan Marcotte",
+        "description": "This classic book covers the principles of web standards and how to use them effectively in web design. It is a must-read for anyone serious about web development.",
+        "best_for": "Developers and designers interested in learning about web standards and best practices in CSS.",
+        "amazon_link": "https://www.amazon.co.uk/dp/0321616952",
+        "cover_image": "https://m.media-amazon.com/images/I/51yKAFtOpPL._SX396_BO1,204,203,200_.jpg"
+    }
+]
