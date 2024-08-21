92dcdb50db2a215e8633cf3f9b85f7a0768dd9eb
Ciaran Mc Devitt <cmcdgit@gmail.com>
2024-08-21 21:06:23 +0000

    WIP: questions and possible answer roughly diplaying to the question area, some comments need to be tidied up

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,11 @@ import { cssBookRecommendations } from "../recommended-books/css-book-recommenda
 import { javascriptBookRecommendations } from "../recommended-books/javascript-book-recommendations.js";
 import { cPlusPlusBookRecommendations } from "../recommended-books/cPlusPlus-book-recommendations.js";
 
+let pythonQuestionsAsked = [];
+let cssQuestionsAsked = [];
+let javascriptQuestionsAsked = [];
+let cPlusPlusQuestionsAsked = [];
+
 document.addEventListener("DOMContentLoaded", function() {
     let options = document.getElementsByTagName('button');
 
@@ -23,43 +28,71 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function takeQuiz(language) {
-    let questionNum = Math.floor(Math.random() * 29);
 
     if (language === "python") {
-        takePythonQuiz(questionNum);
+        // takePythonQuiz();
+        displayQuestion(pythonQuestionsAsked, pythonQuestions);
     } else if (language === "css") {
-        takeCSSQuiz(questionNum);
+        // takeCSSQuiz();
+        displayQuestion(cssQuestionsAsked, cssQuestions);
     } else if (language === "javascript") {
-        takeJavaScriptQuiz(questionNum);
+        // takeJavaScriptQuiz();
+        displayQuestion(javascriptQuestionsAsked, javascriptQuestions);
     } else if (language === "c++") {
-        takeCPPQuiz(questionNum);
+        // takeCPPQuiz();
+        displayQuestion(cPlusPlusQuestionsAsked, cPlusPlusQuestions);
     }
 }
 
-function takePythonQuiz(questionNum) {
-    console.log(questionNum);
-    console.log(pythonQuestions[questionNum]);
-    console.log(pythonQuestions);
-    console.log(pythonBookRecommendations);
+function getUniqueRandomNumberUnder29(askedArray) {
+    let questionNum
+    do {
+        questionNum = Math.floor(Math.random() * 29);
+    } while (askedArray.includes(questionNum));
+
+    askedArray.push(questionNum);
+    return questionNum;
 }
 
-function takeCSSQuiz(questionNum) {
-    console.log(questionNum);
-    console.log(cssQuestions[questionNum]);
-    console.log(cssQuestions);
-    console.log(cssBookRecommendations);
+function displayQuestion(questionsAsked, questions) {
+    let questionBox = document.getElementById("questionBox");
+    questionBox.style.display = "block";
+
+    let randomNum = getUniqueRandomNumberUnder29(questionsAsked);
+    // console.log("questions[randomNum]['question']", questions[randomNum]['options'][0]);
+
+    document.getElementById("question").textContent = questions[randomNum]['question'];
+    document.getElementById("option1").textContent = questions[randomNum]['options'][0];
+    document.getElementById("option2").textContent = questions[randomNum]['options'][1];
+    document.getElementById("option3").textContent = questions[randomNum]['options'][2];
+    document.getElementById("option4").textContent = questions[randomNum]['options'][3];
 }
 
-function takeJavaScriptQuiz(questionNum) {
-    console.log(questionNum);
-    console.log(javascriptQuestions[questionNum]);
-    console.log(javascriptQuestions);
-    console.log(javascriptBookRecommendations);
-}
+// TODO: remove below when troubleshooting complete
+// function takePythonQuiz() {
+//     displayQuestion(pythonQuestionsAsked, pythonQuestions);
+//     // console.log("pythonQuestions\n", pythonQuestions);
+//     // console.log("pythonQuestionsAsked\n", pythonQuestionsAsked);
+//     // console.log("pythonBookRecommendations\n", pythonBookRecommendations);
+// }
 
-function takeCPPQuiz(questionNum) {
-    console.log(questionNum);
-    console.log(cPlusPlusQuestions[questionNum]);
-    console.log(cPlusPlusQuestions);
-    console.log(cPlusPlusBookRecommendations);
-}
+// function takeCSSQuiz() {
+//     displayQuestion(cssQuestionsAsked, cssQuestions);
+//     // console.log("cssQuestions\n", cssQuestions);
+//     // console.log("cssQuestionsAsked\n", cssQuestionsAsked);
+//     // console.log("cssBookRecommendations\n", cssBookRecommendations);
+// }
+
+// function takeJavaScriptQuiz() {
+//     displayQuestion(javascriptQuestionsAsked, javascriptQuestions);
+//     // console.log("javascriptQuestions\n", javascriptQuestions);
+//     // console.log("javascriptQuestionsAsked\n", javascriptQuestionsAsked);
+//     // console.log("javascriptBookRecommendations\n", javascriptBookRecommendations);
+// }
+
+// function takeCPPQuiz() {
+//     displayQuestion(cPlusPlusQuestionsAsked, cPlusPlusQuestions);
+//     // console.log("cPlusPlusQuestions", cPlusPlusQuestions);
+//     // console.log("cPlusPlusQuestionsAsked", cPlusPlusQuestionsAsked);
+//     // console.log("cPlusPlusBookRecommendations", cPlusPlusBookRecommendations);
+// }
